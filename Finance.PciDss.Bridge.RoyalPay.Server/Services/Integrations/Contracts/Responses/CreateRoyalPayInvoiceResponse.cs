87481d3859b931611bc7dd148bc53d3865dd967f
Yuriy Plyaskin <yuriy.p@smplt.net>
2022-04-18 12:33:29 +0300

    Fix texcent

--- Finance.PciDss.Bridge.RoyalPay.Server/Services/Integrations/Contracts/Responses/CreateRoyalPayInvoiceResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace Finance.PciDss.Bridge.RoyalPay.Server.Services.Integrations.Contracts.Responses
-{
-    public class CreateRoyalPayInvoiceResponse
-    {
-        [JsonProperty("transaction")] public CreateRoyalPayInvoiceResponseTransaction Transaction { get; set; }
-    }
-}
--- Finance.PciDss.Bridge.RoyalPay.Server/Services/Integrations/Contracts/Responses/CreateRoyalPayInvoiceResponseAvsVerification.cs
+++
@@ -1,9 +0,0 @@
-﻿using Newtonsoft.Json;
-
-namespace Finance.PciDss.Bridge.RoyalPay.Server.Services.Integrations.Contracts.Responses
-{
-    public class CreateRoyalPayInvoiceResponseAvsVerification
-    {
-        [JsonProperty("result_code")] public string ResultCode { get; set; }
-    }
-}
